b02115cf2060ef18766cb8a9c4ee8be70c52ca59
Mehdi Avara <avaramehdi@gmail.com>
2022-05-17 21:08:13 +0430

    splash screen navigator to homePage

--- lib/Pages/splash Screen.dart
+++ lib/Pages/splash Screen.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 
+import 'HomePage.dart';
+
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key key}) : super(key: key);
 
   @override
   State<SplashScreen> createState() => _SplashScreenState();
+
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  @override
+  initState() {
+    super.initState();
+    _navigateToHome();
+  }
+  _navigateToHome()async{
+    await Future.delayed(Duration(seconds: 3), () {});
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => MyHomePage(title: "hello")));
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
