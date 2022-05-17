ff01b69f787b2132c23315e87c6c789ba7268df0
Mehdi Avara <avaramehdi@gmail.com>
2022-05-17 20:50:05 +0430

    replace HomeScreen with splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:ui_flutter/Pages/splash%20Screen.dart';
 import 'Pages/HomePage.dart';
 
 void main() {
-  runApp(const HomePage());
+  runApp(const SplashScreen());
 }
