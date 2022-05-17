add5c41badd3c576a99ca5117c68d127a5062e84
Morteza Mahdavi <98619035+MortezaMahdavi2002@users.noreply.github.com>
2022-05-18 00:20:10 +0430

    morteza login

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,4 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:ui_flutter/screens/login/login_screen.dart';
+import 'package:ui_flutter/config/palette.dart';
+
+class LoginSignUpScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    bool isSignUpScreen = true;
+    bool isRememberMe = false;
+    bool isMale = true;
+
+    Widget build(BuildContext context){
+      return Scaffold(
+        backgroundColor: Palette.backgroundColor,
+        body: Stack(
+          children: [
+            Positioned(
+              top:0,
+              child:Container(
+                height: 300,
+                decoration: BoxDecoration(
+                  // image:DecorationImage("images/background.png"),
+                ),
 
 
+              )
+            )
+          ],
+        )
+
+      );
+    }
+  }
+}
