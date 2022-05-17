3419de30345d78169b9a112bfe1f435c0af36636
Mehdi Avara <avaramehdi@gmail.com>
2022-05-17 21:26:03 +0430

    delete comment

--- lib/main.dart
+++ lib/main.dart
@@ -3,28 +3,17 @@ import 'package:ui_flutter/Pages/splash%20Screen.dart';
 
 
 void main() {
-  runApp(const HomePage());
+  runApp(const App());
 }
-class HomePage extends StatelessWidget {
+class App extends StatelessWidget {
 
-  const HomePage({Key key}) : super(key: key);
+  const App({Key key}) : super(key: key);
 
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Reddit',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const SplashScreen(),
