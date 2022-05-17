a56784f37969b092fd6d7ffb448d539177f845f7
Morteza Mahdavi <98619035+MortezaMahdavi2002@users.noreply.github.com>
2022-05-18 01:52:21 +0430

    set login page

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -2,12 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:ui_flutter/screens/login/login_screen.dart';
 import 'package:ui_flutter/config/palette.dart';
 
+
 class LoginSignUpScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     bool isSignUpScreen = true;
     bool isRememberMe = false;
     bool isMale = true;
+
+
 
     Widget build(BuildContext context){
       return Scaffold(
@@ -20,10 +23,30 @@ class LoginSignUpScreen extends StatelessWidget {
                     height: 300,
                     decoration: BoxDecoration(
                       color: Colors.black,
-                      // image:DecorationImage("images/background.png"),
+                      image:DecorationImage(
+                        image: AssetImage("images/background.jpg"),
+                        fit: BoxFit.fill,
+
+                      )
                     ),
-
-
+                    child: Container(
+                      padding: EdgeInsets.only(top: 90,left: 20),
+                      color: Color(0xFF3b5999).withOpacity(.85),
+                      child: Column(
+                        children: [
+                          RichText(
+                            text: TextSpan(
+                              text: 'Welcome to Reddit',
+                              style: TextStyle(
+                                fontSize: 25,
+                                color: Colors.yellow[700],
+                                fontWeight: FontWeight.bold,
+                              ),
+                              )
+                            )
+                        ],
+                      )
+                    )
                   )
               )
             ],
