c9f2177371caec24afc93fbdff3834b1cc6e6b85
Morteza Mahdavi <98619035+MortezaMahdavi2002@users.noreply.github.com>
2022-05-17 23:34:09 +0430

    morteza_Login

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:ui_flutter/Pages/splash%20Screen.dart';
+import 'package:ui_flutter/screens/login/login_screen.dart';
 
 
 void main() {
   runApp(const App());
+  runApp(LoginPage());
 }
+class LoginSignUpUI extends StatelessWidget {
+  const LoginSignUpUI ({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Login SignUp UI',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginSignUpPage(),
+    );
+  }
+}
+
+
+
 class App extends StatelessWidget {
 
   const App({Key key}) : super(key: key);
