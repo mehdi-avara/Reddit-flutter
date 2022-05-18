fa8b04c44bc1a1ea370921a4bbb7932a3283df7f
Mehdi Avara <avaramehdi@gmail.com>
2022-05-18 23:59:36 +0430

    update post model

--- lib/models/post.dart
+++ lib/models/post.dart
@@ -2,15 +2,46 @@ import 'package:ui_flutter/models/community.dart';
 import 'package:ui_flutter/models/user.dart';
 
 class post{
-  String postId;
-  String title;
-  String discription;
-  List<String> imageUrl;
-  User user;
-  Community community;
+  String _postId;
+  String _title;
+  String _discription;
+  List<String> _imageUrl;
+  User _user;
+  Community _community;
 
+  String get postId => _postId;
 
+  Community get community => _community;
 
+  User get user => _user;
 
+  List<String> get imageUrl => _imageUrl;
 
+  String get discription => _discription;
+
+  String get title => _title;
+
+  set postId(String value) {
+    _postId = value;
+  }
+
+  set community(Community value) {
+    _community = value;
+  }
+
+  set user(User value) {
+    _user = value;
+  }
+
+  set imageUrl(List<String> value) {
+    _imageUrl = value;
+  }
+
+  set discription(String value) {
+    _discription = value;
+  }
+
+  set title(String value) {
+    _title = value;
+  }
 }
