29d8ac844cca4950ed41182da1e27725deaed14c
Mehdi Avara <avaramehdi@gmail.com>
2022-05-18 15:37:04 +0430

    add some theme consts to consts

--- lib/config/palette.dart
+++ lib/config/palette.dart
@@ -11,4 +11,15 @@ class Palette{
   static const Color widgetBackground = Color(0xFF474747);
 
 }
+class themeSizes{
+  static const double fontTitle1 = 18.0;
+  static const double fontTitle2 = 16.0;
+  static const double fontParagraph1 = 14.0;
+  static const double fontParagraph2 = 12.0;
+  static const double fontParagraph3 = 10.0;
+  static const double communityName = 14.0;
+  static const double userName = 12.0;
+  static const double avatarIconSize = 25.0;
+  static const double avatarRadius = 17.0;
+}
 
