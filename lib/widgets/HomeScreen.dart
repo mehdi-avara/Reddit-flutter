5858cc0bcbd8ac819d30671046e4f60a2aeda228
Mehdi Avara <avaramehdi@gmail.com>
2022-05-18 13:45:37 +0430

    widget inesialeser

---
+++ lib/widgets/HomeScreen.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class HomeWidget extends StatelessWidget {
+  const HomeWidget({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        ),
+
+      child: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                  color: Colors.red,
+                  height: 100,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.green,
+                  height: 100,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.blue,
+                  height: 100,
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                  color: Colors.red,
+                  height: 100,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.green,
+                  height: 100,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.blue,
+                  height: 100,
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                  color: Colors.red,
+                  height: 100,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.green,
+                  height: 100,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.blue,
+                  height: 100,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
