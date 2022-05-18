9012439f86aa96b3cbf2d74bd9c9155475d417dd
Mehdi Avara <avaramehdi@gmail.com>
2022-05-18 15:19:55 +0430

    add widget to homescreen

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -77,6 +77,7 @@ class _MyHomePageState extends State<MyHomePage> {
               style: Theme.of(context).textTheme.headline4,
             ),
             HomeWidget(),
+            HomeWidget(),
           ],
         ),
       ),
