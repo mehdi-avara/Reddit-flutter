ee1a1e846133ddfc07b79a470d264cdf82c04de6
Mehdi Avara <avaramehdi@gmail.com>
2022-05-18 23:54:01 +0430

    add models

---
+++ lib/models/post.dart
@@ -0,0 +1,16 @@
+import 'package:ui_flutter/models/community.dart';
+import 'package:ui_flutter/models/user.dart';
+
+class post{
+  String postId;
+  String title;
+  String discription;
+  List<String> imageUrl;
+  User user;
+  Community community;
+
+
+
+
+
+}
