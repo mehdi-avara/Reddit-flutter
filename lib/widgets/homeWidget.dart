c0202dadbf374bb10330561babb3de3bb1f50420
Mehdi Avara <avaramehdi@gmail.com>
2022-05-19 10:37:19 +0430

    bring functions to homeWidget

--- lib/widgets/homeWidget.dart
+++ lib/widgets/homeWidget.dart
@@ -14,24 +14,22 @@ class HomeWidget extends StatelessWidget {
 
       child: Column(
         children: <Widget>[
-          homeWidget._profileListTile(_post.user.name, _post.community.name, _post.user.profileImageUrl),
-          homeWidget._postPart(_post.title,_post.discription),
-homeWidget._belowPostPart(),
+          _profileListTile(_post.user.name, _post.community.name, _post.user.profileImageUrl),
+          _postPart(_post.title,_post.discription),
+          _belowPostPart(),
         ],
       ),
     );
   }
-}
-class homeWidget{
-  static Container _postPart(String Title,String description){
+  Container _postPart(String Title,String description){
     return Container(
       child: ListTile(
         title: Text(Title,style: TextStyle(fontSize: themeSizes.communityName,fontWeight: FontWeight.bold,color: Palette.textColor1),),
         subtitle: Text(description,style: TextStyle(fontSize: themeSizes.userName,color: Palette.textColor2),),
       ),
-      );
+    );
   }
-  static Container _profileListTile(String title, String subtitle, String imagePath) {
+  Container _profileListTile(String title, String subtitle, String imagePath) {
     return Container(
       child: ListTile(
         leading: CircleAvatar(
@@ -67,32 +65,32 @@ class homeWidget{
       ),
     );
   }
-  static Container _belowPostPart(){
+  Container _belowPostPart(){
     return Container(
       child:Row(
         children: <Widget>[
           Expanded(
             child: Container(
-                // color: Colors.green,
-                height: 30,
-                child: Row(
-                  children: <Widget>[
-                    IconButton(
-                      icon:Icon(Icons.arrow_upward
-                        ,color: Palette.textColor2,
-                      ),
-                      onPressed: (){},
-                      iconSize: 15,),
-                    Text("vote",style: TextStyle(color: Colors.white,fontSize: 10),),
-                    IconButton(
-                      icon:Icon(Icons.arrow_downward
-                        ,color: Palette.textColor2,
-                      ),
-                      onPressed: (){},
-                      iconSize: 15,),
-                  ],
-                ),
+              // color: Colors.green,
+              height: 30,
+              child: Row(
+                children: <Widget>[
+                  IconButton(
+                    icon:Icon(Icons.arrow_upward
+                      ,color: Palette.textColor2,
+                    ),
+                    onPressed: (){},
+                    iconSize: 15,),
+                  Text("vote",style: TextStyle(color: Colors.white,fontSize: 10),),
+                  IconButton(
+                    icon:Icon(Icons.arrow_downward
+                      ,color: Palette.textColor2,
+                    ),
+                    onPressed: (){},
+                    iconSize: 15,),
+                ],
               ),
+            ),
           ),
           Expanded(
             child: Container(
@@ -137,5 +135,8 @@ class homeWidget{
       ),
     );
   }
+}
+class homeWidget{
+
 
 }
