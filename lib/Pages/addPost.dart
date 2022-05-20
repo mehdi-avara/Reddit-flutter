04e659eda63d344379cc9cb73314167b4b601c2b
Morteza Mahdavi <98619035+MortezaMahdavi2002@users.noreply.github.com>
2022-05-20 17:34:49 +0430

    add page first textfield

--- lib/Pages/addPost.dart
+++ lib/Pages/addPost.dart
@@ -7,9 +7,24 @@ class addPost extends StatefulWidget {
   State<addPost> createState() => _addPostState();
 }
 
+
+
 class _addPostState extends State<addPost> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Add a Title'),
+        ),
+        body: Padding(
+            padding: EdgeInsets.all(15),
+            child: Column(
+                children: <Widget> [
+
+
+                ]
+            )
+        )
+    );
   }
 }
