f28d73b44870285c4d004c6d7535e3099f0bdc9c
Morteza Mahdavi <98619035+MortezaMahdavi2002@users.noreply.github.com>
2022-05-20 17:14:09 +0430

    create addPage file

---
+++ lib/Pages/addPost.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class addPost extends StatefulWidget {
+  const addPost({Key key}) : super(key: key);
+
+  @override
+  State<addPost> createState() => _addPostState();
+}
+
+class _addPostState extends State<addPost> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
