bd4411829e8bc7f44146b419cde629e0a052300e
Morteza Mahdavi <98619035+MortezaMahdavi2002@users.noreply.github.com>
2022-05-21 23:02:02 +0430

    two option of setting is built

--- lib/Pages/SettingPage.dart
+++ lib/Pages/SettingPage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:ui_flutter/Pages/addPost.dart';
+import 'package:ui_flutter/Pages/feedPage.dart';
+import 'package:ui_flutter/Pages/loginSignUpPage.dart';
 import 'package:ui_flutter/widgets/otherWidget.dart';
 import '../config/themeSettings.dart';
 
@@ -26,61 +29,76 @@ class _SettingPageState extends State<SettingPage> {
             },
           ),
         ),
-        body: Center(
-          child: ListView(
-            children: <Widget>[
-              IconButton(onPressed: (){} ,icon: Icon(Icons.settings),),
-              RaisedButton(onPressed: (){
-                Navigator.pop(context);
-              },
-                child: Text('Other Widget'),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  border: Border(
-                    bottom: BorderSide(
-                      style: BorderStyle.solid,
-                      color: Colors.grey,
-                      width: 1.0,
-                    ),
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/setting.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Center(
+            child: ListView(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(top: 20.0, bottom: 20.0, left: 5.0, right: 250.0),
+                  child: Container(
+                      child:TextButton.icon(
+                          label: Text('Profile', style: TextStyle(color: Colors.grey, fontSize: 20.0),),
+                          icon: Icon(Icons.person,color: Colors.red,size: 40,),
+                          onPressed: () {
+                            print('Pressed');
+                          }
+                      ),
                   ),
                 ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 2.0, bottom: 5.0, left: 20.0, right: 180.0),
+                  child: Container(
+                    // padding: EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0, bottom: 20.0),
+                    child: TextButton.icon(
+                      label: Text('Create Community', style: TextStyle(fontSize: 20.0,color: Colors.grey),),
+                      icon: Icon(Icons.people,size: 40,color: Colors.yellow,),
+                      onPressed: () {
+                        print('Pressed');
+                      },
+                    ),
 
-                padding: EdgeInsets.only(top: 10.0, bottom: 30.0, left: 10.0, right: 10.0),
-                child: Text('Account settings',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-              ),
-              Container(
-                padding: EdgeInsets.all(10),
-                child: Text('Theme',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-              ),
-              Container(
-                padding: EdgeInsets.all(10),
-                child: Text('Language',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-              ),
-              Container(
-                padding: EdgeInsets.all(10),
-                child: Text('About',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-              ),
-              Container(
-                padding: EdgeInsets.all(10),
-                child: Text('Contact',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-              ),
-              Container(
-                padding: EdgeInsets.all(10),
-                child: Text('Privacy Policy',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-              ),
-              Container(
-                padding: EdgeInsets.all(10),
-                child: Text('Terms of Service',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-              ),
-              Container(
-                padding: EdgeInsets.all(10),
-                child: Text('Logout',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-              ),
-            ],
-          ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    child: Text('Language',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: Text('About',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: Text('Contact',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: Text('Privacy Policy',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: Text('Terms of Service',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: Text('Logout',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                ),
+              ],
+            ),
+
         ),
         // bottomNavigationBar: widgets.bottomNavigationBar(context),
+      ),
       ),
     );
   }
