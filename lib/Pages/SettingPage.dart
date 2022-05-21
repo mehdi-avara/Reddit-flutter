0c85f432e0bbd6deb4a50d65c5e88b1810e1c836
Morteza Mahdavi <98619035+MortezaMahdavi2002@users.noreply.github.com>
2022-05-21 18:16:05 +0430

    building setting page is starting class SettingPage(stateful) and _SettingPageState is build

---
+++ lib/Pages/SettingPage.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import '../config/themeSettings.dart';
+
+
+class SettingPage extends StatefulWidget {
+  @override
+  _SettingPageState createState() => _SettingPageState();
+}
+
+class _SettingPageState extends State<SettingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode:ThemeMode.system,
+      theme: ThemeClass.darkTheme,
+      darkTheme: ThemeClass.darkTheme,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Setting'),
+        ),
+        body: Center(
+          child: Text('Setting'),
+        ),
+      ),
+    );
+  }
+}
