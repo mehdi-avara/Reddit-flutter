fb643d287fe17dcf25003326ca2d08d52ef8ba03
Mehdi Avara <avaramehdi@gmail.com>
2022-05-21 18:45:25 +0430

    community page ended

--- lib/Pages/communityPage.dart
+++ lib/Pages/communityPage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:ui_flutter/config/themeSettings.dart';
+import 'package:ui_flutter/models/community.dart';
 import 'package:ui_flutter/models/user.dart';
+import 'package:ui_flutter/widgets/communityWidget.dart';
 import 'package:ui_flutter/widgets/otherWidget.dart';
 
 class communityPage extends StatefulWidget {
@@ -12,6 +14,12 @@ class communityPage extends StatefulWidget {
 
 class _communityPageState extends State<communityPage> {
   User _user = User.withProfileImageUrl("Sourav", "assets/images/profile.jpg");
+  List<Community> _community = [
+    new Community.withProfileImageUrl("Flutter", "assets/images/flutter.png"),
+    new Community.withProfileImageUrl("Dart", "assets/images/dart.png"),
+    new Community.withProfileImageUrl("React", "assets/images/react.png"),
+    new Community.withProfileImageUrl("Vue", "assets/images/vue.png"),
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -51,7 +59,12 @@ class _communityPageState extends State<communityPage> {
           ),
         ),
         body: Center(
-
+        child: ListView.builder(
+            itemCount: _community.length,
+            itemBuilder: (BuildContext context, int index) {
+              return communityWidget(_community[index], key: Key(index.toString()));
+            },
+          ),
         ),
         bottomNavigationBar: widgets.bottomNavigationBar(context),
       ),
