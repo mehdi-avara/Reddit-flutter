cc798c60f3e4615d347d116cc93343198ef8eddd
Mehdi Avara <avaramehdi@gmail.com>
2022-05-22 21:07:45 +0430

    add SingleChildScrollView to addPostPage

--- lib/Pages/addPost.dart
+++ lib/Pages/addPost.dart
@@ -50,81 +50,83 @@ class _addPostState extends State<addPost> {
           ],
         ),
 
-    body: Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('assets/images/AddPostTheme.jpg'),
-          fit: BoxFit.cover,
+    body: SingleChildScrollView(
+      child: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/AddPostTheme.jpg'),
+            fit: BoxFit.cover,
+          ),
         ),
-      ),
-      child: Padding(
-        padding: EdgeInsets.only(top: 140.0, left: 40.0, right: 40.0,bottom: 10.0),
-              child:Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 20.0, bottom: 20.0, left: 30.0, right: 30.0),
-                    child: Row(
-                      children: [
-                        Text('Choose The Community',style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold),),
-                        IconButton(
-                          icon: Icon(Icons.list),
-                          onPressed: () {
-                            ExpansionPanelList(
-                              expansionCallback: (int index, bool isExpanded) {},
-                              children: [
-                                ExpansionPanel(
-                                  headerBuilder: (BuildContext context, bool isExpanded) {
-                                    return ListTile(
-                                      title: Text('Item 1'),
-                                    );
-                                  },
-                                  body: ListTile(
-                                    title: Text('Item 1 child'),
-                                    subtitle: Text('Details goes here'),
+        child: Padding(
+          padding: EdgeInsets.only(top: 140.0, left: 40.0, right: 40.0,bottom: 10.0),
+                child:Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20.0, bottom: 20.0, left: 30.0, right: 30.0),
+                      child: Row(
+                        children: [
+                          Text('Choose The Community',style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold),),
+                          IconButton(
+                            icon: Icon(Icons.list),
+                            onPressed: () {
+                              ExpansionPanelList(
+                                expansionCallback: (int index, bool isExpanded) {},
+                                children: [
+                                  ExpansionPanel(
+                                    headerBuilder: (BuildContext context, bool isExpanded) {
+                                      return ListTile(
+                                        title: Text('Item 1'),
+                                      );
+                                    },
+                                    body: ListTile(
+                                      title: Text('Item 1 child'),
+                                      subtitle: Text('Details goes here'),
+                                    ),
+                                    isExpanded: true,
                                   ),
-                                  isExpanded: true,
-                                ),
-                                ExpansionPanel(
-                                  headerBuilder: (BuildContext context, bool isExpanded) {
-                                    return ListTile(
-                                      title: Text('Item 2'),
-                                    );
-                                  },
-                                  body: ListTile(
-                                    title: Text('Item 2 child'),
-                                    subtitle: Text('Details goes here'),
+                                  ExpansionPanel(
+                                    headerBuilder: (BuildContext context, bool isExpanded) {
+                                      return ListTile(
+                                        title: Text('Item 2'),
+                                      );
+                                    },
+                                    body: ListTile(
+                                      title: Text('Item 2 child'),
+                                      subtitle: Text('Details goes here'),
+                                    ),
+                                    isExpanded: false,
                                   ),
-                                  isExpanded: false,
-                                ),
-                              ],
-                            );
-                          },
-                        ),
-                      ],
+                                ],
+                              );
+                            },
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                  ListTile(
-                    title: TextField(
-                      decoration: InputDecoration(
-                          labelText: 'Title',
-                          hintText: 'Add a Title',
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(20.0))),
-                    ),
-                    subtitle: Padding(
-                      padding: const EdgeInsets.only(top: 30.0, bottom: 60.0, left: 0.0, right: 0.0),
-                      child: TextField(
+                    ListTile(
+                      title: TextField(
                         decoration: InputDecoration(
-                            labelText: 'Description',
-                            hintText: 'Add a Description',
+                            labelText: 'Title',
+                            hintText: 'Add a Title',
                             border: OutlineInputBorder(
                                 borderRadius: BorderRadius.circular(20.0))),
                       ),
+                      subtitle: Padding(
+                        padding: const EdgeInsets.only(top: 30.0, bottom: 60.0, left: 0.0, right: 0.0),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              labelText: 'Description',
+                              hintText: 'Add a Description',
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20.0))),
+                        ),
+                      ),
                     ),
-                  ),
-                ],
-              ),
-          ),
+                  ],
+                ),
+            ),
+      ),
     ),
       ),
 
