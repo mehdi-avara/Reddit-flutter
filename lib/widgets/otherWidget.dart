0bd360f4b44c76252d3e7842ae603af652c98a91
Mehdi Avara <avaramehdi@gmail.com>
2022-05-22 15:55:58 +0430

    cleanup the code

--- lib/widgets/otherWidget.dart
+++ lib/widgets/otherWidget.dart
@@ -13,6 +13,7 @@ class widgets extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container();
   }
+
   static BottomNavigationBar bottomNavigationBar(BuildContext context) {
     return BottomNavigationBar(
       items: const <BottomNavigationBarItem>[
@@ -26,7 +27,6 @@ class widgets extends StatelessWidget {
           icon: Icon(
             Icons.group,
           ),
-
           label: 'community',
         ),
         BottomNavigationBarItem(
@@ -34,7 +34,6 @@ class widgets extends StatelessWidget {
             Icons.add,
           ),
           label: 'Add',
-
         ),
         BottomNavigationBarItem(
           icon: Icon(
@@ -50,63 +49,65 @@ class widgets extends StatelessWidget {
         ),
       ],
       onTap: (index) {
-        onTapFunction(index,context);
+        onTapFunction(index, context);
       },
       showUnselectedLabels: true,
-
       showSelectedLabels: true,
     );
   }
-  static onTapFunction(int mode,BuildContext context){
-    switch(mode){
+
+  static onTapFunction(int mode, BuildContext context) {
+    switch (mode) {
       case 0:
-      Navigator.push(context, MaterialPageRoute(builder: (context)=>feedPage()));
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => feedPage()));
         break;
       case 1:
-      Navigator.push(context, MaterialPageRoute(builder: (context)=>communityPage()));
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => communityPage()));
         break;
       case 2:
-        Navigator.push(context,MaterialPageRoute(builder: (context)=>addPost()));
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => addPost()));
         break;
       case 3:
-      // Navigator.push(context, MaterialPageRoute(builder: (context)=>ChatPage()));
+        // Navigator.push(context, MaterialPageRoute(builder: (context)=>ChatPage()));
         break;
       case 4:
-      Navigator.push(context, MaterialPageRoute(builder: (context)=>SettingPage()));
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => SettingPage()));
         break;
     }
   }
-static AppBar appBar(User _user){
+
+  static AppBar appBar(User _user) {
     return AppBar(
-
-  title: ListTile(
-  leading: CircleAvatar(
-  radius: themeSizes.avatarRadius,
-  child: Image.asset(_user.profileImageUrl),
-  ),
-  title: TextField(
-  decoration: InputDecoration(
-  hintText: "Search",
-  hintStyle: TextStyle(
-  fontSize: themeSizes.searchBarHint,
-  ),
-  border: InputBorder.none,
-  suffixIcon: IconButton(
-  icon: Icon(Icons.search),
-  onPressed: (){},
-  ),
-
-  ),
-  ),
-  trailing: IconButton(
-  icon: Icon(
-  Icons.view_headline,
-  size: themeSizes.searchBarIcon,
-  ),
-  onPressed: () {},
-  ),
-  ),
-  );
-}
-
+      title: ListTile(
+        leading: CircleAvatar(
+          radius: themeSizes.avatarRadius,
+          child: Image.asset(_user.profileImageUrl),
+        ),
+        title: TextField(
+          decoration: InputDecoration(
+            hintText: "Search",
+            hintStyle: TextStyle(
+              fontSize: themeSizes.searchBarHint,
+            ),
+            border: InputBorder.none,
+            suffixIcon: IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
+            ),
+          ),
+        ),
+        trailing: IconButton(
+          icon: Icon(
+            Icons.view_headline,
+            size: themeSizes.searchBarIcon,
+          ),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
 }
