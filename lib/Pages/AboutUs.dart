a5d3a43a9f134164a9215aedfd59b150eb412a9b
Morteza Mahdavi <98619035+MortezaMahdavi2002@users.noreply.github.com>
2022-05-23 15:19:48 +0430

    about us page

--- lib/Pages/AboutUs.dart
+++ lib/Pages/AboutUs.dart
@@ -8,7 +8,27 @@ class AboutUs extends StatelessWidget {
         title: Text('About Us'),
       ),
       body: Center(
-        child: Text('About Us'),
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/AboutUs2.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: ListView(
+            children: <Widget>[
+              Container(
+                child: Text(
+                  'Mehdi Avara',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+          ),
+          ]
+        ),
+      ),
       ),
     );
   }
