8fe087779a86f7019ddb7310ee5b506ab2e5c8cb
Mehdi Avara <avaramehdi@gmail.com>
2022-05-25 20:39:38 +0430

    make files correct

--- lib/Pages/Profile.dart
+++ lib/Pages/Profile.dart
@@ -31,74 +31,63 @@ class _ProfileState extends State<Profile> {
             ),
             child: ListView(
               children: <Widget>[
-                Center(
-                  // padding: EdgeInsets.only(top: 20.0, bottom: 20.0, left: 20.0, right: 20.0),
-                  child: Container(
-                    padding: EdgeInsets.only(top: 20.0, bottom: 20.0, left: 20.0, right: 280.0),
-                    child: Text(
-                      'Username : ',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
+                Container(
+                  padding: EdgeInsets.only(top: 20.0, bottom: 20.0, left: 20.0, right: 0.0),
+                  child: Text(
+                    'Username : ',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
-                Center(
-                  // padding: EdgeInsets.all(10),
-                  child: Container(
-                    padding: EdgeInsets.only(top: 20.0, bottom: 20.0, left: 20.0, right: 280.0),
-                    child: Text(
-                      'Password : ',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
+                Container(
+                  padding: EdgeInsets.only(top: 20.0, bottom: 20.0, left: 20.0, right: 0.0),
+                  child: Text(
+                    'Password : ',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
-                Center(
-                  // ,
-                  child: Container(
-                    padding: EdgeInsets.only(top: 20.0, bottom: 20.0, left: 20.0, right: 280.0),
-                    child: Text(
-                      'Email : ',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
+                Container(
+                  padding: EdgeInsets.only(top: 20.0, bottom: 20.0, left: 20.0, right: 0.0),
+                  child: Text(
+                    'Email : ',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
-                Center(
-                  child: Container(
-                    padding: EdgeInsets.only(top: 90.0, bottom: 80.0, left: 20.0, right: 20.0),
-                    child: SizedBox(
-                      width: 200,
-                      height: 50,
-                      child: TextButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => EditInfo()),
-                          );
-                        },
-                        style: TextButton.styleFrom(
-                          primary: Colors.white,
-                          backgroundColor: Colors.blueGrey,
-                          onSurface: Colors.grey,
-                        ),
-                        child: Text(
-                          'Edit Profile',
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                          ),
+                Container(
+                  padding: EdgeInsets.only(top: 90.0, bottom: 80.0, left: 20.0, right: 20.0),
+                  child: SizedBox(
+                    width: 200,
+                    height: 50,
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => EditInfo()),
+                        );
+                      },
+                      style: TextButton.styleFrom(
+                        primary: Colors.white,
+                        backgroundColor: Colors.blueGrey,
+                        onSurface: Colors.grey,
+                      ),
+                      child: Text(
+                        'Edit Profile',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
                     ),
                   ),
-                  )
+                )
               ],
             ),
           ),
