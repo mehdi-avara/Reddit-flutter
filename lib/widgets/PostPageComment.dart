d697ba5d1bf8d4f6eb20a538521af93ab8352a9f
Mehdi Avara <avaramehdi@gmail.com>
2022-05-27 00:34:13 +0430

    post page comment part widget starts

---
+++ lib/widgets/PostPageComment.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PostPageComment extends StatelessWidget {
+  const PostPageComment({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
